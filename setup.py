103f1f4a6b5e6b7ee947722591e4a38487053df4
f-ld <f.ld@free.fr>
2018-02-09 17:25:22 +0000

    Update dependency from airflow to apache-airflow

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     description='Alternative LDAP auth backend for airflow to support openLDAP installation without memberOf overlay',
     long_description=long_description,
@@ -73,7 +73,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['airflow'],
+    install_requires=['apache-airflow'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
